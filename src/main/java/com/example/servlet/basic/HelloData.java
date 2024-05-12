eda673c05dde73b1fcca9134a07beda7722f7763
idohyeon <ehgus5026@naver.com>
2024-05-12 18:22:27 +0900

    -Json parsing Object

---
+++ src/main/java/com/example/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package com.example.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class HelloData {
+
+    private String userName;
+    private int age;
+
+}
