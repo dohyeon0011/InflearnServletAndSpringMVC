0c2db2f0ac6b03d834af03f1982e6161d4f88ad1
idohyeon <ehgus5026@naver.com>
2024-05-13 22:22:47 +0900

    -Front ControllerV1(uri mapping)

---
+++ src/main/java/com/example/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -0,0 +1,13 @@
+package com.example.servlet.web.frontcontroller.v1;
+
+import com.example.servlet.web.frontcontroller.MyView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface ControllerV1 {
+
+    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
---
+++ src/main/java/com/example/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,33 @@
+package com.example.servlet.web.frontcontroller.v1.controller;
+
+import com.example.servlet.domain.member.Member;
+import com.example.servlet.domain.member.MemberRepository;
+import com.example.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("userName");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(userName, age);
+        memberRepository.save(member);
+
+        // Model에 데이터를 보관 후 넘김
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
+
