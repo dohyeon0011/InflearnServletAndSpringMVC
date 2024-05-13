449497c5d47dc53f7b0537a0b8442949d441c389
idohyeon <ehgus5026@naver.com>
2024-05-13 22:23:34 +0900

    -Front ControllerV2(uri mapping + forward)

---
+++ src/main/java/com/example/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,32 @@
+package com.example.servlet.web.frontcontroller.v2.controller;
+
+import com.example.servlet.domain.member.Member;
+import com.example.servlet.domain.member.MemberRepository;
+import com.example.servlet.web.frontcontroller.MyView;
+import com.example.servlet.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("userName");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(userName, age);
+        memberRepository.save(member);
+
+        // Model에 데이터를 보관 후 넘김
+        request.setAttribute("member", member);
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
+    }
+}
+
