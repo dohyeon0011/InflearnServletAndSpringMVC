edf47620fc8ecfcbab0df81dea9254ced9dcc55a
idohyeon <ehgus5026@naver.com>
2024-05-14 17:25:07 +0900

    -V3(Model to RequestAttribute)

--- src/main/java/com/example/servlet/web/frontcontroller/MyView.java
+++ src/main/java/com/example/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -18,5 +19,15 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);    // 모델에 있는 데이터를 request Attribute로 바꾸기, 렌더가 들어오면 모델에 있는 값을 모두 꺼내서 request setAttribute에 넣어서 jsp-forward가 끝나면 jsp가 request getAttribute를 씀
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
